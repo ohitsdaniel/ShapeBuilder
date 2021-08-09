4b72e46fbc100cc29ed64766da999839d5534332
Daniel Peter <daniel.peter@me.com>
2021-08-09 14:44:42 +0200

    Unhide dependencies

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
     ),
   ],
   dependencies: [
-//    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
-//    .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing", .upToNextMajor(from: "1.9.0")) // dev
+    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.0"), // dev
+    .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing", .upToNextMajor(from: "1.9.0")) // dev
   ],
   targets: [
     .target(
       name: "ShapeBuilder",
       dependencies: []
     ),
-//    .testTarget(name: "ShapeBuilderTests", dependencies: ["ShapeBuilder", "SnapshotTesting"], exclude: ["__Snapshots__"]) // dev
+    .testTarget(name: "ShapeBuilderTests", dependencies: ["ShapeBuilder", "SnapshotTesting"], exclude: ["__Snapshots__"]) // dev
   ]
 )
 
