fb8a42747d55cb1dd35ae08ca4793094b192db7a
Daniel Peter <daniel.peter@me.com>
2021-09-07 09:48:49 +0200

    Drop swift-tools-version down to 5.3

--- Sources/ShapeBuilder/ShapeBuilder.swift
+++ Sources/ShapeBuilder/ShapeBuilder.swift
@@ -1,20 +1,31 @@
 import SwiftUI
 
+#if swift(>=5.4)
 @resultBuilder
 public enum ShapeBuilder {
   public static func buildBlock<S: Shape>(_ builder: S) -> some Shape {
     builder
   }
+}
+#else
+@_functionBuilder
+public enum ShapeBuilder {
+  public static func buildBlock<S: Shape>(_ builder: S) -> some Shape {
+    builder
+  }
+}
+#endif
 
-  public static func buildOptional<S: Shape>(_ component: S?) -> EitherShape<S, EmptyShape> {
+public extension ShapeBuilder {
+  static func buildOptional<S: Shape>(_ component: S?) -> EitherShape<S, EmptyShape> {
     component.flatMap(EitherShape.first) ?? EitherShape.second(EmptyShape())
   }
 
-  public static func buildEither<First: Shape, Second: Shape>(first component: First) -> EitherShape<First, Second> {
+  static func buildEither<First: Shape, Second: Shape>(first component: First) -> EitherShape<First, Second> {
     .first(component)
   }
 
-  public static func buildEither<First: Shape, Second: Shape>(second component: Second) -> EitherShape<First, Second> {
+  static func buildEither<First: Shape, Second: Shape>(second component: Second) -> EitherShape<First, Second> {
     .second(component)
   }
 }
