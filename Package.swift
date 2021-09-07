9c3fb831b10471d9f8ff46bde855fbcf29be6b28
Daniel Peter <daniel.peter@me.com>
2021-09-07 09:48:49 +0200

    Drop swift-tools-version down to 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -23,7 +23,8 @@ let package = Package(
       dependencies: []
     ),
     .testTarget(name: "ShapeBuilderTests", dependencies: ["ShapeBuilder", "SnapshotTesting"], exclude: ["__Snapshots__"]) // dev
-  ]
+  ],
+  swiftLanguageVersions: [.v5]
 )
 
 #if canImport(PackageConfig)
--- Sources/ShapeBuilder/InsettableShapeBuilder.swift
+++ Sources/ShapeBuilder/InsettableShapeBuilder.swift
@@ -1,22 +1,33 @@
 import SwiftUI
 
+#if swift(>=5.4)
 @resultBuilder
 public enum InsettableShapeBuilder {
   public static func buildBlock<S: InsettableShape>(_ builder: S) -> some InsettableShape {
     builder
   }
+}
+#else
+@_functionBuilder
+public enum InsettableShapeBuilder {
+  public static func buildBlock<S: InsettableShape>(_ builder: S) -> some InsettableShape {
+    builder
+  }
+}
+#endif
 
-  public static func buildOptional<S: InsettableShape>(_ component: S?) -> EitherInsettableShape<S, EmptyShape> {
+public extension InsettableShapeBuilder {
+  static func buildOptional<S: InsettableShape>(_ component: S?) -> EitherInsettableShape<S, EmptyShape> {
     component.flatMap(EitherInsettableShape.first) ?? .second(EmptyShape())
   }
 
-  public static func buildEither<First: InsettableShape, Second: InsettableShape>(
+  static func buildEither<First: InsettableShape, Second: InsettableShape>(
     first component: First
   ) -> EitherInsettableShape<First, Second> {
     .first(component)
   }
 
-  public static func buildEither<First: InsettableShape, Second: InsettableShape>(
+  static func buildEither<First: InsettableShape, Second: InsettableShape>(
     second component: Second
   ) -> EitherInsettableShape<First, Second> {
     .second(component)
